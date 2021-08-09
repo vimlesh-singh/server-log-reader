58c6b2bbf6cd7dcfcc9c515b2c5a1006f27306ef
vimleshsinghk <vimleshsinghk@gmail.com>
2021-08-09 12:33:12 +0100

    Added function to read file and get data as an array

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,50 @@ import './App.css';
 import Input from './components/input/Input';
 
 function App() {
-  const handleInputChange = (e: any) => {
-    console.log(e);
+  const readFile = (file: any) => {
+    // Check if the file type is .log.
+    console.log(file);
+    // if (file.name && !file.name.endsWith('.log')) {
+    //   console.log('File is not supported.', file.type, file);
+    //   alert(`'File is not supported.', ${file.type}, ${file.name}`)
+    //   return;
+    // }
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.addEventListener('progress', event => {
+      if (event.loaded && event.total) {
+        const percent = (event.loaded / event.total) * 100;
+        console.log(`Progress: ${Math.round(percent)}`);
+      }
+    });
+    reader.addEventListener('load', event => {
+      const result = event.target?.result;
+      const dataArray = String(result).split(/\n/);
+      const visitedPages: string[] = [];
+      const uniqueVisitedPages: string[] = [];
+      const visitedIpList: string[] = [];
+      dataArray.forEach(val => {
+        let pageVisitIndex = val.search(/\s/);
+        if (pageVisitIndex > -1) {
+          let visitedPage = val.substring(0, pageVisitIndex);
+          visitedPages.push(visitedPage);
+          if (!visitedIpList.includes(val)) {
+            visitedIpList.push(val);
+            uniqueVisitedPages.push(visitedPage);
+          }
+        }
+      });
+      console.log({ visitedPages, uniqueVisitedPages });
+    });
+    reader.readAsText(file);
   };
+
+  const handleInputChange = (e: any) => {
+    readFile(e.target.files[0]);
+  };
+
   return (
     <div className='App' data-testid='main-app'>
       <header className='App-header'>
